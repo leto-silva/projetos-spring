a897b8aa13c8f7b28365819d91b9c04c4d32ec73
WellingtonEugenio <wellingtoneugenio@gmail.com>
2024-06-11 14:50:05 -0300

    Iniciando o projetos-spring

---
+++ src/main/java/one/digitalinnovation/gof/service/ClienteService.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.gof.service;
+
+import one.digitalinnovation.gof.model.Cliente;
+
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente. Com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma interface
+ * 
+ * @author eugenio.silva
+ */
+
+public interface ClienteService {
+	
+	Iterable<Cliente> buscarTodos();
+	
+	Cliente buscarPorId(Long Id);
+	
+	void inserir(Cliente cliente);
+	
+	void atualizar(Long Id, Cliente cliente);
+	
+	void deletar(Long Id);
+
+}
